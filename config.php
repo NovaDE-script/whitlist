500525c40903578e87882aabcdc0c8c3764bd4f8
NovaDE-script <6u5z4r432@gmail.com>
2025-08-05 20:38:22 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php 
+$config = [
+    'auth' => [
+        'auth_client_id' => 'your_discord_bot_client_id',
+        'auth_client_secret' => 'your_discord_bot_secret',
+        'auth_redirect_index' => 'https://youtsite.com/index',
+        'auth_redirect_admin' => 'https://yoursite.com/admin'
+    ],
+    'db' => [
+        'db_host' => 'host',
+        'db_user' => 'user',
+        'db_name' => 'name',
+        'db_password' => 'password'
+
+    ]
+
+]
+?>
